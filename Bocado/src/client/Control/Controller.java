29371186313f06dd687c274788a98e3b61c9dbd3
tinshake <hermansson.gustaf@gmail.com>
2020-05-07 14:50:50 +0200

    added reader for ingredients

--- Bocado/src/client/Control/Controller.java
+++ Bocado/src/client/Control/Controller.java
@@ -7,7 +7,10 @@ import client.View.NorthPanel;
 import server.recipeHandler.Recipe;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Controller {
 
@@ -17,14 +20,25 @@ public class Controller {
         mainframe=new MainFrame(this);
     }
 
-    public void add(String ingredients){
-
-    }
-
     public void filterRecipes(String filter){
         ArrayList<Recipe> recipes=new ServerConnection("localhost",2500).sendRequest(filter);
         CenterPanel cPanel = mainframe.getCenterpanel();
         cPanel.setRecipeList(recipes);
+    }
+
+    public void readIngredients(){
+        try {
+            File file = new File("files2/Ingredients.txt");
+            Scanner myReader = new Scanner(file);
+            while (myReader.hasNextLine()) {
+                String info = myReader.nextLine();
+                System.out.println(info);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 
 }
