0d213b05eccbadbdf4de95ffd5ed5698eea3c508
tinshake <hermansson.gustaf@gmail.com>
2020-05-27 22:37:27 +0200

    rec

--- Bocado/src/client/View/MainFrame.java
+++ Bocado/src/client/View/MainFrame.java
@@ -16,7 +16,7 @@ public class MainFrame extends JFrame {
         public void start(){
 
             setSize(800,600);
-            setTitle("Bocado");
+            setTitle("Bocado v1.0");
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             setLocation(150,100);
             panel = new MainPanel(controller);
